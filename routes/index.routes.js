4735c9eca1b1c192811a5861dc330df630943892
Lex <owner@Owners-MBP.attlocal.net>
2022-04-27 21:49:20 -0400

    Styled signin and login views. created 6 static view pages and connected all the GET routes.

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,8 +4,28 @@ const isLoggedIn = require('../middleware/isLoggedIn')
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+// GET about us page
+router.get("/aboutUs", (req, res, next) => {
+  res.render("about-us");
+});
+//GET service page
+router.get("/services", (req, res, next) => {
+  res.render("services");
+});
+//GET client portal page
+router.get("/clientPortal", (req, res, next) => {
+  res.render("client-portal");
+});
+//GET location page
+router.get("/contactUs", (req, res, next) => {
+  res.render("contact-us");
+});
+//GET location page
+router.get("/location", (req, res, next) => {
+  res.render("location");
+});
 
-// verifies that user is logged in to have access to myProfile view
+// verifies that user is \/ \/ \/ \/ logged in to have access to myProfile view
 router.get("/myProfile", isLoggedIn,(req,res, next)=>{
   res.render("user-profile");
 })
